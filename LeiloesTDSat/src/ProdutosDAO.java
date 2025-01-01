c84e3b8901d53d097ba63245312f12855a69c158
Fortune Aristote Audate <faudate05@gmail.com>
2025-01-01 15:01:12 -0300

    Implementacao e atualizacao da funcinalidade salvar no sistema.

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -12,22 +12,38 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
 public class ProdutosDAO {
     
     Connection conn;
-    PreparedStatement prep;
-    ResultSet resultset;
+    PreparedStatement st;
+    ResultSet rs; 
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
+    try{ 
+   
+       
+    
+        st=conn.prepareStatement("INSERT INTO produtos VALUES(?,?,?,?)");
+        st.setInt(1,produto.getId());
+        st.setString(2,produto.getNome());
+        st.setInt(3, produto.getValor()); 
+        st.setString(4,produto.getStatus());
+      
+       
+   
+       }catch(SQLException ex) {
+            System.out.println("Erro ao conectar: " + ex.getMessage());
+           
         
         
-        //conn = new conectaDAO().connectDB();
         
         
+    } 
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
